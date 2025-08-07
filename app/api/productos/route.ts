dcb637888e75c68974ca81850baf16e41dd94cdf
Rene Enriquez <enriquezrene.ap@gmail.com>
2025-08-07 10:23:26 -0500

    seeders

--- app/api/productos/route.ts
+++ app/api/productos/route.ts
@@ -19,7 +19,7 @@ export async function OPTIONS() {
 export async function GET(request: Request) {
   const url = new URL(request.url);
   const page = parseInt(url.searchParams.get("page") || "1");
-  const itemsPerPage = 20;
+  const itemsPerPage = parseInt(url.searchParams.get("items") || "20");
   const offset = (page - 1) * itemsPerPage;
 
   const items = await prisma.producto.findMany({
@@ -33,80 +33,4 @@ export async function GET(request: Request) {
 
   return NextResponse.json({ items, totalPages },
     { headers: corsHeaders });
-}
-
-
-export async function PUT(request: Request) {
-  console.log("PUT request received");
-  /*const page = parseInt(url.searchParams.get("page") || "1");
-  const postsPerPage = 5;
-  const offset = (page - 1) * postsPerPage;
-
-  // Fetch paginated posts
-  const posts = await prisma.post.findMany({
-    skip: offset,
-    take: postsPerPage,
-    orderBy: { createdAt: "desc" },
-    include: { author: { select: { name: true } } },
-  });
-
-  const totalPosts = await prisma.post.count();
-  const totalPages = Math.ceil(totalPosts / postsPerPage);*/
-
-  return NextResponse.json({
-    products: [
-      { id: 1, name: "Product 1", description: "Description for product 1" },
-      { id: 2, name: "Product 2", description: "Description for product 2" },
-      { id: 3, name: "Product 3", description: "Description for product 3" }
-    ], totalCount: 1 });
-}
-
-export async function POST(request: Request) {
-  console.log("POST request received");
-  /*const page = parseInt(url.searchParams.get("page") || "1");
-  const postsPerPage = 5;
-  const offset = (page - 1) * postsPerPage;
-
-  // Fetch paginated posts
-  const posts = await prisma.post.findMany({
-    skip: offset,
-    take: postsPerPage,
-    orderBy: { createdAt: "desc" },
-    include: { author: { select: { name: true } } },
-  });
-
-  const totalPosts = await prisma.post.count();
-  const totalPages = Math.ceil(totalPosts / postsPerPage);*/
-
-  return NextResponse.json({
-    products: [
-      { id: 1, name: "Product 1", description: "Description for product 1" },
-      { id: 2, name: "Product 2", description: "Description for product 2" },
-      { id: 3, name: "Product 3", description: "Description for product 3" }
-    ], totalCount: 1 });
-}
-
-export async function DELETE(request: Request) {
-  console.log("DELETE request received");
-  /*const page = parseInt(url.searchParams.get("page") || "1");
-  const postsPerPage = 5;
-  const offset = (page - 1) * postsPerPage;
-
-  // Fetch paginated posts
-  const posts = await prisma.post.findMany({
-    skip: offset,
-    take: postsPerPage,
-    orderBy: { createdAt: "desc" },
-    include: { author: { select: { name: true } } },
-  });
-
-  const totalPosts = await prisma.post.count();
-  const totalPages = Math.ceil(totalPosts / postsPerPage);*/
-
-  return NextResponse.json({
-    products: [
-      { id: 1, name: "Product 1", description: "Description for product 1" },
-      { id: 2, name: "Product 2", description: "Description for product 2" },
-      { id: 3, name: "Product 3", description: "Description for product 3" }
-    ], totalCount: 1 });
 }
